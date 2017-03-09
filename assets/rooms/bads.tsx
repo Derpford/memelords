c35be230e3a535b56c8f5210757e51c0f2081a4b
Chris Scott <cascott1994@gmail.com>
2017-03-09 18:37:41 +0000

    added enemies to all rooms

---
+++ assets/rooms/bads.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bads" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <properties>
+  <property name="bad" value="hood"/>
+ </properties>
+ <image source="../tiles/bads.png" width="64" height="64"/>
+</tileset>
