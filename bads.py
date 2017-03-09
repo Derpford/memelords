012cd500507ec494dfd81348827f4d0c0e8864bb
Chris Scott <cascott1994@gmail.com>
2017-03-09 17:32:31 +0000

    begin work on enemy update code

--- bads.py
+++ bads.py
@@ -25,3 +25,16 @@ class Bad(actors.Actor):
         self.maxhp=3
         self.dead=False
         self.pattern=[(1,0),(-1,0)]
+        self.patternTimer=0.5
+        self.patternTimerMax=0.5
+        self.patternStep=-1
+
+    def update(self):
+        self.t+=self.dt
+        self.patternTimer-=dt
+        if self.patternTimer<0:
+            self.patternStep+=1
+            self.dx, self.dy=self.pattern[self.patternStep]
+            self.patternTimer=self.patternTimerMax
+        self.body.apply_force_at_local_point((self.dx*actors.factor,self.dy*actors.factor),(0,0))
+
