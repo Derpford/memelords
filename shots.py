5f7b9ac5ed86b85d89c1f2b9b2429113b6d66539
Chris Scott <cascott1994@gmail.com>
2017-03-17 00:31:23 +0100

    Added spread weapons

--- shots.py
+++ shots.py
@@ -58,3 +58,36 @@ class BadShot(Shot):
     def __init__(self,space,x,y,fx,fy,speed=120,damage=1,dt=1/120):
         Shot.__init__(self,space,x,y,fx,fy,speed,damage,dt)
         self.shape.collision_type=collisionTypes["badshot"]
+
+# Spreader weapon.
+class SpreadShot(Shot):
+    def __init__(self,space,x,y,fx,fy,speed=160,damage=1,dt=1/120):
+        Shot.__init__(self,space,x,y,fx,fy,speed,damage,dt)
+        self.timer=0.45
+        self.anim=[loadImage('assets/shots/pulser1.png'),
+                    loadImage('assets/shots/pulser2.png'),
+                    loadImage('assets/shots/pulser3.png'),]
+
+# Spreader sub-shot.
+class SubShot(Shot):
+    def __init__(self,space,x,y,fx,fy,speed=160,damage=1,dt=1/120):
+        Shot.__init__(self,space,x,y,fx,fy,speed,damage,dt)
+        self.timer=0.40
+        if abs(fy)>abs(fx):
+            self.anim=[ pygame.transform.rotate(loadImage('assets/shots/beam1.png'),90),
+                    pygame.transform.rotate(loadImage('assets/shots/beam2.png'),90),
+                    pygame.transform.rotate(loadImage('assets/shots/beam3.png'),90),]
+        else:
+            self.anim=[ loadImage('assets/shots/beam1.png'),
+                    loadImage('assets/shots/beam2.png'),
+                    loadImage('assets/shots/beam3.png'),]
+
+class BadSpreadShot(SpreadShot):
+    def __init__(self,space,x,y,fx,fy,speed=120,damage=1,dt=1/120):
+        SpreadShot.__init__(self,space,x,y,fx,fy,speed,damage,dt)
+        self.shape.collision_type=collisionTypes["badshot"]
+
+class BadSubShot(SubShot):
+    def __init__(self,space,x,y,fx,fy,speed=120,damage=1,dt=1/120):
+        SubShot.__init__(self,space,x,y,fx,fy,speed,damage,dt)
+        self.shape.collision_type=collisionTypes["badshot"]
--- weapons.py
+++ weapons.py
@@ -49,6 +49,58 @@ class Sword(Weapon):
         Weapon.__init__(self)
         self.maxShot=2
 
+class Axe(Weapon):
+    def __init__(self):
+        Weapon.__init__(self)
+        self.name="Axe"
+        self.maxShot=1
+        self.anim=loadImage('assets/weapons/axe.png')
 
+    def shoot(self,space,pos,face,parent):
+        # Spread shot.
+        if len(parent.shotList)<self.maxShot:
+            self.face=face[:]
+            angle=math.atan2(self.face[1],self.face[0])
+            angleL=angle+math.pi*0.125
+            angleR=angle-math.pi*0.125
+            faceLeft=math.cos(angleL),math.sin(angleL)
+            faceRight=math.cos(angleR),math.sin(angleR)
+            newShot=shots.SpreadShot(space,pos.x+16*self.face[0],pos.y+16*self.face[1],self.face[0],self.face[1])
+            parent.shotList.append(newShot)
+            spreadShot=shots.SubShot(space,pos.x+16*self.face[0],pos.y+16*self.face[1],faceLeft[0],faceLeft[1])
+            parent.shotList.append(spreadShot)
+            spreadShot=shots.SubShot(space,pos.x+16*self.face[0],pos.y+16*self.face[1],faceRight[0],faceRight[1])
+            parent.shotList.append(spreadShot)
+            return True
+        else: return False
 
+    def draw(self,surf,pos,weaponAnim):
+        if debugFlags["anim"]:
+            print(str(math.floor(pos[0]+self.face[0]*weaponAnim*16))+","+str(math.floor(pos[1]+self.face[1]*weaponAnim*16))+" anim pos for "+str(self.anim))
+            print("("+str(pos[0])+"+"+str(self.face[0])+"*"+str(weaponAnim)+"*16)")
+            print("("+str(pos[1])+"+"+str(self.face[1])+"*"+str(weaponAnim)+"*16)")
+        weaponPos=(pos[0]+self.face[0]*weaponAnim*16,pos[1]+self.face[1]*weaponAnim*16)
+        anim=pygame.transform.flip(self.anim,self.face[0]<0,False)
+        surf.blit(anim,weaponPos)
 
+class BadAxe(Axe):
+    def __init__(self):
+        Axe.__init__(self)
+
+    def shoot(self,space,pos,face,parent):
+        # Spread shot.
+        if len(parent.shotList)<self.maxShot:
+            self.face=face[:]
+            angle=math.atan2(self.face[1],self.face[0])
+            angleL=angle+math.pi*0.125
+            angleR=angle-math.pi*0.125
+            faceLeft=math.cos(angleL),math.sin(angleL)
+            faceRight=math.cos(angleR),math.sin(angleR)
+            newShot=shots.BadSpreadShot(space,pos.x+16*self.face[0],pos.y+16*self.face[1],self.face[0],self.face[1])
+            parent.shotList.append(newShot)
+            spreadShot=shots.BadSubShot(space,pos.x+16*self.face[0],pos.y+16*self.face[1],faceLeft[0],faceLeft[1])
+            parent.shotList.append(spreadShot)
+            spreadShot=shots.BadSubShot(space,pos.x+16*self.face[0],pos.y+16*self.face[1],faceRight[0],faceRight[1])
+            parent.shotList.append(spreadShot)
+            return True
+        else: return False
