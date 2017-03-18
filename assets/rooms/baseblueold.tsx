56a5f942843932bc3e52b00cbd8a65c68cd75306
Chris Scott <cascott1994@gmail.com>
2017-03-18 05:48:49 +0100

    more resources I forgot to add initially

---
+++ assets/rooms/baseblueold.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="baseblueold" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="../tiles/baseblueold.png" width="64" height="64"/>
+ <tile id="1" probability="0.01"/>
+ <tile id="2" probability="0.01"/>
+ <tile id="3" probability="0.01"/>
+ <tile id="4" probability="0.02"/>
+ <tile id="5" probability="0.005"/>
+ <tile id="6" probability="0.005"/>
+ <tile id="7" probability="0.005"/>
+ <tile id="8" probability="0.005"/>
+ <tile id="9" probability="0.005"/>
+ <tile id="10" probability="0.005"/>
+ <tile id="11" probability="0.005"/>
+ <tile id="12" probability="0.003"/>
+ <tile id="13" probability="0.003"/>
+ <tile id="14" probability="0.003"/>
+ <tile id="15" probability="0.003"/>
+</tileset>
