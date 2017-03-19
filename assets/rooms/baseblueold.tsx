d550f311e38c47944ffd8a49bfd9ad75aa667400
Chris Scott <cascott1994@gmail.com>
2017-03-19 04:56:14 +0100

    tweaked tile randomness

--- assets/rooms/baseblueold.tsx
+++ assets/rooms/baseblueold.tsx
@@ -1,7 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="baseblueold" tilewidth="16" tileheight="16" tilecount="16" columns="4">
  <image source="../tiles/baseblueold.png" width="64" height="64"/>
- <tile id="1" probability="0.01"/>
+ <tile id="1" probability="0.01">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="2" probability="0.01"/>
  <tile id="3" probability="0.01"/>
  <tile id="4" probability="0.02"/>
