7de6532d584599dd765abb56d3bcd4e82bad3dfc
BuildTools <unconfigured@null.spigotmc.org>
2019-04-04 13:34:07 -0500

    One too many numbers there.

--- actors.py
+++ actors.py
@@ -30,7 +30,8 @@ class Actor(pygame.sprite.Sprite):
         self.name=None
         self.dead=False
         pygame.sprite.Sprite.__init__(self)
-        self.body=pymunk.Body(1,1,float('inf')) # Magic numbers!
+        self.body=pymunk.Body(1,1) # Magic numbers!
+        print(self.body)
         self.body.position=(x,y)
         #self.shape=pymunk.Circle(self.body,8)
         self.anim = [loadImage("assets/guy-green/guy-green1.png"),
