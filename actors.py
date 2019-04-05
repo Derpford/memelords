d291a3203470da318386eaa32af8c229216a6d87
BuildTools <unconfigured@null.spigotmc.org>
2019-04-05 17:21:16 -0500

    debug prints leave this place

--- actors.py
+++ actors.py
@@ -31,7 +31,7 @@ class Actor(pygame.sprite.Sprite):
         self.dead=False
         pygame.sprite.Sprite.__init__(self)
         self.body=pymunk.Body(1,1) # Magic numbers!
-        print(self.body)
+        #print(self.body)
         self.body.position=(x,y)
         #self.shape=pymunk.Circle(self.body,8)
         self.anim = [loadImage("assets/guy-green/guy-green1.png"),
